478233745dccd034dfce2d13b942c03294e8118c
zubayer-rahman <zubayer5566@gmail.com>
2025-12-11 14:59:51 +0600

    updated the homepage fetcing from the resume logs

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,20 +1,36 @@
 import React from "react";
 import { Link } from "react-router";
+import ScoreCircle from "./ScoreCircle";
 
-const ResumeCard = ({ resume }: { resume: Resume }) => {
+const ResumeCard = ({ resume: {id, companyName, jobTitle, feedback, imagePath} }: { resume: Resume }) => {
   return (
     <Link
-      to={`/resume/${resume.id}`}
+      to={`/resume/${id}`}
       className="resume-card animate-in fade-in duration-1000"
     >
-        <div className="flex flex-col">
-            <h2 className="text-black! font-bold wrap-break-word">
-                {resume.companyName}
-            </h2>
+        <div className="resume-card-header">
+            <div className="flex flex-col gap-2">
+                <h2 className="text-black! font-bold wrap-break-word">
+                    {companyName}
+                </h2>
 
-            <h3 className="text-grey-500 text-lg wrap-break-word">
-                {resume.jobTitle}
-            </h3>
+                <h3 className="text-grey-500 text-lg wrap-break-word">
+                    {jobTitle}
+                </h3>
+            </div>
+            <div className="shrink-0">
+                <ScoreCircle score={feedback.overallScore} />
+            </div>
+        </div>
+
+        <div className="gradient-border animate-in fade-in duration-1000">
+            <div className="w-full h-full">
+                <img 
+                    src={imagePath}
+                    alt="resume"
+                    className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+                />
+            </div>
         </div>
     </Link>
   );
